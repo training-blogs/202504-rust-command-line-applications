5f7e2b0638983bc167d14df3fec6c880d11278be
Alexis THOMAS <fr.alexisthomas@gmail.com>
2025-04-27 01:28:13 +0200

    1.6 Testing

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -12,15 +12,37 @@ struct Cli {
 
 fn main() -> Result<()> {
     let args = Cli::parse();
-
     let content = std::fs::read_to_string(&args.path)
         .with_context(|| format!("could not read file `{}`", args.path.display()))?;
 
-    for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("{}", line);
-        }
-    }
+    find_matches(&content, &args.pattern, &mut std::io::stdout())?;
 
     Ok(())
+}
+
+fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) -> Result<()> {
+    for line in content.lines() {
+        if line.contains(pattern) {
+            writeln!(writer, "{}", line)?;
+        }
+    }
+    Ok(())
+}
+
+#[test]
+fn find_a_match() -> Result<()> {
+    let mut result = Vec::new();
+    find_matches("lorem ipsum\ndolor sit amet", "lorem", &mut result)?;
+    assert_eq!(result, b"lorem ipsum\n");
+    Ok(())
+}
+
+#[allow(dead_code)]
+fn answer() -> i32 {
+    42
+}
+
+#[test]
+fn check_answer_validity() {
+    assert_eq!(answer(), 42);
 }
