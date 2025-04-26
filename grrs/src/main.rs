a85346f734e868d0f65f1138376c998fcf81cd30
Alexis THOMAS <fr.alexisthomas@gmail.com>
2025-04-26 23:12:19 +0200

    1.2 Parsing command-line arguments

--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,3 +1,46 @@
+// Getting the arguments
+
+/*fn main() {
+    let pattern = std::env::args().nth(1).expect("no pattern given");
+    let path = std::env::args().nth(2).expect("no path given");
+
+    println!("pattern: {:?}, path: {:?}", pattern, path)
+}*/
+
+// CLI arguments as data types
+
+/*struct Cli {
+    pattern: String,
+    path: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let pattern = std::env::args().nth(1).expect("no pattern given");
+    let path = std::env::args().nth(2).expect("no path given");
+
+    let args = Cli {
+        pattern,
+        path: std::path::PathBuf::from(path),
+    };
+
+    println!("pattern: {:?}, path: {:?}", args.pattern, args.path);
+}*/
+
+// Parsing CLI arguments with Clap
+
+use clap::Parser;
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
+struct Cli {
+    /// The pattern to look for
+    pattern: String,
+    /// The path to the file to read
+    path: std::path::PathBuf,
+}
+
+fn main() {
+    let args = Cli::parse();
+
+    println!("pattern: {:?}, path: {:?}", args.pattern, args.path)
 }
