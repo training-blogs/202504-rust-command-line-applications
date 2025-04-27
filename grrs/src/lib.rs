14bc2933590a12408d781d1b699831b3156a2190
Alexis THOMAS <fr.alexisthomas@gmail.com>
2025-04-27 17:55:17 +0200

    1.6 - update Result and Error

--- grrs/src/lib.rs
+++ grrs/src/lib.rs
@@ -2,7 +2,7 @@ pub fn find_matches(
     content: &str,
     pattern: &str,
     mut writer: impl std::io::Write,
-) -> Result<(), std::io::Error> {
+) -> Result<(), Box<dyn std::error::Error>> {
     for line in content.lines() {
         if line.contains(pattern) {
             writeln!(writer, "{}", line)?;
@@ -12,7 +12,7 @@ pub fn find_matches(
 }
 
 #[test]
-fn find_a_match() -> Result<(), std::io::Error> {
+fn find_a_match() -> Result<(), Box<dyn std::error::Error>> {
     let mut result = Vec::new();
     find_matches("lorem ipsum\ndolor sit amet", "lorem", &mut result)?;
     assert_eq!(result, b"lorem ipsum\n");
--- grrs/src/main.rs
+++ grrs/src/main.rs
@@ -1,4 +1,4 @@
-use anyhow::{Context, Result};
+use anyhow::Context;
 use clap::Parser;
 
 /// Search for a pattern in a file and display the lines that contain it.
@@ -10,7 +10,7 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn main() -> Result<()> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::parse();
     let content = std::fs::read_to_string(&args.path)
         .with_context(|| format!("could not read file `{}`", args.path.display()))?;
